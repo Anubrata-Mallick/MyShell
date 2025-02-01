557356febdbafe87cbbba4a04dfc8504d1ea684d
Anubrata Mallick <anubrata2003snr@outlook.com>
2025-02-01 17:49:50 +0530

    Implement of exit 0

--- app/main.py
+++ app/main.py
@@ -1,16 +1,18 @@
 import sys
 
-
 def main():
     # Create a REPL (Read-Execute-Print-Loop)
     while True :
         # Uncomment this block to pass the first stage
         sys.stdout.write("$ ")
-        # Wait for user input
+        # Wait for user input ------ READ 
         command = input()
-
-        # Print every command as invalid 
-        print(f"{command}: command not found")
+        #--------------------------- EXECUTE
+        match command :
+            case "exit 0":
+                break
+            case _ :
+                print(f"{command}: command not found")
 
 if __name__ == "__main__":
     main()
