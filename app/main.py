e4156d10444a3468bd3f32ae025889dd0c0c7377
Anubrata Mallick <anubrata2003snr@outlook.com>
2025-02-01 17:37:02 +0530

    REPL

--- app/main.py
+++ app/main.py
@@ -5,11 +5,13 @@ def main():
     # Uncomment this block to pass the first stage
     sys.stdout.write("$ ")
 
-    # Wait for user input
-    command = input()
+    # Create a REPL (Read-Execute-Print-Loop)
+    while True :
+        # Wait for user input
+        command = input()
 
-    # Print every command as invalid 
-    print(f"{command}: command not found")
+        # Print every command as invalid 
+        print(f"{command}: command not found")
 
 if __name__ == "__main__":
     main()
