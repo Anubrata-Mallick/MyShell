62e5171c115ccbbda6f65bb48d06ddb08f7adeca
Anubrata Mallick <anubrata2003snr@outlook.com>
2025-02-01 19:54:17 +0530

    Improve type command v1

--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
 import sys
+import os
+
 COMMANDS = ["echo", "exit", "type"]
+PATH = os.environ.get("PATH")
 
 def main():
         
@@ -9,11 +12,22 @@ def main():
     command = input()
     match command :
         case command if command.startswith("type "):
-            keyword = command.split(maxsplit=1)[1]
-            if keyword in COMMANDS:
-                sys.stdout.write(f"{keyword} is a shell builtin \n")
+
+            cmd = command.split(maxsplit=1)[1]
+            cmd_path = None
+            paths = PATH.split(":")
+
+            for path in paths:
+                if os.path.isfile(f"{path}/{cmd}"):
+                    cmd_path = f"{path}/{cmd}"
+
+            if cmd in COMMANDS:
+                sys.stdout.write(f"{cmd} is a shell builtin \n")
+            elif cmd_path:
+                sys.stdout.write(f"{cmd} is {cmd_path}\n")
             else:
-                sys.stdout.write(f"{keyword}: not found \n")
+                sys.stdout.write(f"{cmd}: not found \n")
+
         case command if command.startswith("echo "):
             sys.stdout.write(f"{command[ len("echo "): ]}\n")
         case "exit 0":
