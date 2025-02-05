f05583362cb1a2e46b86a7412ef20bca082aba67
Anubrata Mallick <anubrata2003snr@outlook.com>
2025-02-05 11:10:41 +0530

    Unstable v1

--- app/main.py
+++ app/main.py
@@ -7,6 +7,9 @@ import shlex
 COMMANDS = ["echo", "exit", "type", "pwd", "cd"]
 PATH = os.environ.get("PATH", "")
 
+def print_to_shell(output)-> None:
+    sys.stdout.write(output)
+
 def locate_executable(command) -> Optional[str]:
 
     for directory in PATH.split(":"):
@@ -15,7 +18,7 @@ def locate_executable(command) -> Optional[str]:
         if os.path.isfile(file_path) and os.access(file_path, os.X_OK):
             return command
 
-def handle_cd(command):
+def handle_cd(command) -> None:
     # check if user want to go home
     if command.split(maxsplit=1)[1] == "~":
         home_directory = os.path.expanduser("~")
@@ -24,9 +27,9 @@ def handle_cd(command):
     elif os.path.exists(command.split(maxsplit=1)[1]):
         os.chdir(command.split(maxsplit=1)[1]) # change the directory
     else:
-        sys.stdout.write(f"cd: {command.split(maxsplit=1)[1]}: No such file or directory\n")
+        print_to_shell(f"cd: {command.split(maxsplit=1)[1]}: No such file or directory\n")
 
-def handle_type(command):
+def handle_type(command)-> str :
     cmd = command.split(maxsplit=1)[1]
     cmd_path = None
     paths = PATH.split(":")
@@ -35,18 +38,22 @@ def handle_type(command):
         if os.path.isfile(f"{path}/{cmd}"):
             cmd_path = f"{path}/{cmd}"
 
+    output = ""
     if cmd in COMMANDS:
-        sys.stdout.write(f"{cmd} is a shell builtin \n") 
+        output = f"{cmd} is a shell builtin \n"
     elif cmd_path:
-        sys.stdout.write(f"{cmd} is {cmd_path}\n")
+        output = f"{cmd} is {cmd_path}\n"
     else:
-        sys.stdout.write(f"{cmd}: not found \n")
+        output = f"{cmd}: not found \n"
+    return output 
 
-def handle_echo(command):
+def handle_echo(command)->str:
     m = shlex.split(command, posix=True)
-    print(' '.join(m[1:]))
+    output = ' '.join(m[1:])
+    return output
 
-def handle_cat(command, modify=False):
+def handle_cat(command, modify=False)->str:
+    output = ""
     if modify:
         file_paths = shlex.split(command, posix=True)[1:]
     else:
@@ -57,11 +64,43 @@ def handle_cat(command, modify=False):
         try: 
             with open(file_path, "r") as file:
                 content = file.read()
-                sys.stdout.write(f"{content}")
+                output = f"{content}"
         except FileNotFoundError:
-            sys.stdout.write(f"{file_path} Not Found \n")
+            output = f"{file_path} Not Found \n"
         except PermissionError:
-            sys.stdout.write(f"Permission denied for {file_path}\n")
+            output = f"Permission denied for {file_path}\n"
+    return output
+
+def handle_ls(command)-> list[list[str]]:
+    args = shlex.split(command, posix=True)
+    if len(args) == 1:
+        paths = '.'
+    else:
+        paths = args[1:]
+
+    item_box = []
+    for path in paths:
+        item = os.listdir(path)
+        item_box.append(item)
+    return item_box
+
+def Shell_Engine(command)-> str:
+    pass
+
+def handle_pwd()-> str:
+    output = f"{os.getcwd()}\n"
+    return output
+
+# echo 'Hello James' 1> /tmp/foo/foo.md
+def handle_redirect_output(command)->str:
+    if "1>" in command:
+        args = command.split(" 1> ")
+    else:
+        args = command.split(" > ")
+    
+    source, destination = args[0], args[1]
+    
+    source_output = None
 
 
 def main():
@@ -71,20 +110,31 @@ def main():
 
     command = input()
     match command :
+        case command if " 1> " in command or " > " in command:
+            output = handle_redirect_output(command)
+            print_to_shell(output)
         case command if command.startswith("type "):
-            handle_type(command)
+            output = handle_type(command)
+            print_to_shell(output)
         case command if command.startswith("echo "):
-            handle_echo(command)
+            output = handle_echo(command)
+            print_to_shell(output)
         case "exit 0":
             exit(0)
         case "pwd":
-            sys.stdout.write(f"{os.getcwd()}\n")
+            output = handle_pwd()
+            print_to_shell(output)
+        case command if command.startswith("ls ") or command=="ls":
+            output = handle_ls(command)
+            print_to_shell(output)
         case command if command.startswith("cd "):
             handle_cd(command)
         case command if command.startswith("cat "):
-            handle_cat(command)
+            output = handle_cat(command)
+            print_to_shell(output)
         case command if command.startswith("'") or command.startswith('"'):
-            handle_cat(command, modify=True)
+            output = handle_cat(command, modify=True)
+            print_to_shell(output)
         case _:
             if executable := locate_executable(command.split(maxsplit=1)[0]): # execute external executables
                 subprocess.run([executable, command.split(maxsplit=1)[1]])
